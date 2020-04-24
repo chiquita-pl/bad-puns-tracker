b3396a0b2e49356247e24641cde0faa2e7618174
Wiktoria Rybakiewicz <chiquita@podfather.com>
2020-04-24 08:40:57 +0100

    first commit

---
+++ bad-puns-tracker-client/src/main.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+
+Vue.config.productionTip = false;
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import Dashboard from './components/Dashboard.vue'
+import MoviesList from './components/MoviesList.vue'
+
+import Auth from '@okta/okta-vue'
+
+Vue.use(Auth, {
+  issuer: 'https://dev-185110.okta.com/oauth2/default',
+  //issuer: 'https://dev-185110/oauth2/default',
+  client_id: '0oaa30o3i0pwaqCJ14x6',
+  redirect_uri: 'http://localhost:8080/implicit/callback',
+  scope: 'openid profile email'
+})
+
+const routes = [
+  { path: '/implicit/callback', component: Auth.handleCallback() },
+  { path: '/movies', component: MoviesList },
+]
+
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+new Vue({
+  router,
+  render: h => h(Dashboard)
+}).$mount('#app')
+
