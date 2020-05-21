f06e35fae5ff872dbd7a15d8d91dbe7646dcff03
chiquita-pl <cziki50@gmail.com>
2020-05-21 19:41:13 +0100

    Connected frontend with backend

--- .docker/setup.sql
+++ .docker/setup.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE IF NOT EXISTS bad_puns_counter;
-CREATE USER 'bpcuser'@'%' IDENTIFIED BY 'temppass123';
+CREATE USER 'bpcuser'@'%' IDENTIFIED WITH mysql_native_password  BY 'temppass123';
 GRANT ALL PRIVILEGES ON bad_puns_counter.* to bpcuser WITH GRANT OPTION;
--- bad-puns-tracker-server/test.php
+++ bad-puns-tracker-server/test.php
@@ -1,11 +1,13 @@
 <?php
 echo 'Hello' . PHP_EOL;
-$mysqli = new mysqli("docker_mysql_1", "bpcuser", "temppass123");
+$mysqli = new mysqli("badpuns_mysql_1", "bpcuser", "temppass123");
 echo'This is server info: ' . $mysqli->server_info . PHP_EOL;
 
 try {
-	$db = new PDO('mysql:dbname=bac_puns_counter;host=127.0.0.1', 'bpcuser', 'temppass123');
-	echo var_dump($mysqli->query('SELECT user FROM mysql.user'));
+	$db = new PDO('mysql:dbname=bad_puns_counter;host=172.20.0.2', 'bpcuser', 'temppass123');
+	foreach ($db->query('SELECT * FROM movie') as $row) {
+	    echo print_r($row, 1);
+	}
 } catch (PDOException $e) {
 	echo 'Connection failed: ' . $e->getMessage();
 }
